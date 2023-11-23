c95c61822017217077452eb3370f1a09b5c70429
morshed0099 <mdgolammorshed0099@gmail.com>
2023-11-23 20:46:04 +0600

    prettier and eslint setup

---
+++ src/app.ts
@@ -0,0 +1,14 @@
+import express, { Application, Request, Response } from 'express';
+import cors from 'cors';
+const app: Application = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req: Request, res: Response) => {
+  res.status(200).json({
+    status: 'sucess',
+  });
+});
+
+export default app;
